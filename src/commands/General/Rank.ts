d13058f70641dfb9570eff857a1705ca26de763a
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-05-25 23:17:52 +0530

    Update Rank.ts

--- src/commands/General/Rank.ts
+++ src/commands/General/Rank.ts
@@ -34,7 +34,7 @@ export default class Command extends BaseCommand {
 		} catch (err) {
 			M.reply(`Profile Picture not Accessible of ${username}`);
 			pfp =
-				"https://cdn.donmai.us/original/ab/5e/ab5e2ec951546e5df432c9233ef9ab8a.jpg";
+				"https://i.ibb.co/VYfMgR1/8e88411bc7f55cea47c68ac145fe411f.jpg";
 		}
 		const exp = (await this.client.getUser(user)).Xp;
 		let role: string;
