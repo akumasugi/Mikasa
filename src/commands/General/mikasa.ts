17232f1219647d8bc53ebe420c954930b726be79
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-05-26 11:46:49 +0530

    Create mikasa.ts

---
+++ src/commands/General/mikasa.ts
@@ -0,0 +1,34 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "mikasa",
+			description: "Displays the info",
+			category: "general",
+			usage: `${client.config.prefix}elaina`,
+			baseXp: 200,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			"https://c.tenor.com/VCFZnE8WKIwAAAPo/mikasa-ackerman-anime.mp4";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+                                captain:'⚔️*MIKASA*⚔️ \n\n☘️ *Description: repo is private better luck next time.*\n\n⚔️ *insta id: https://www.instagram.com/akuma__24/?hl=en* \n ⚔️\n`,
+			}
+		);
+	};
+}
